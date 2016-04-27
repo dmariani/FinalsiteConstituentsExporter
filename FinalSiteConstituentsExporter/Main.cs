0bf1c0552e0904250c92f1132974a29c508b29ad
unknown <dave@atscale.com>
2016-04-26 18:42:11 -0700

    login is working

--- FinalSiteConstituentsExporter/Main.cs
+++ FinalSiteConstituentsExporter/Main.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Web;
+using System.Net.Http;
+using System.Net;
+using System.IO;
 
 namespace FinalSiteConstituentsExporter
 {
@@ -71,6 +75,49 @@ namespace FinalSiteConstituentsExporter
 
         private Boolean ProcessExport(String Datestr, String Dir)
         {
+            // We use the HttpUtility class from the System.Web namespace  
+            Uri address = new Uri("https://www.ola.community/api/apilogin/apilogin.cfm");
+
+            // Create the web request  
+            HttpWebRequest request = WebRequest.Create(address) as HttpWebRequest;
+
+            // Set type to POST  
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            string username = "david.mariani";
+            string password = "Bully123";
+
+            request.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.Default.GetBytes(username + ":" + password));
+
+            // Create the data we want to send  
+            string key = "0357D83A88AA320BE9194A00D753B5686CBC9547AFD0B6A7E54F9C84B064FBAD";
+
+            StringBuilder data = new StringBuilder();
+            data.Append("ApiEncryptedKey=" + HttpUtility.UrlEncode(key));
+
+            // Create a byte array of the data we want to send  
+            byte[] byteData = UTF8Encoding.UTF8.GetBytes(data.ToString());
+
+            // Set the content length in the request headers  
+            request.ContentLength = byteData.Length;
+
+            // Write data  
+            using (Stream postStream = request.GetRequestStream())
+            {
+                postStream.Write(byteData, 0, byteData.Length);
+            }
+
+            // Get response  
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                // Get the response stream  
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                // Console application output  
+                Console.WriteLine(reader.ReadToEnd());
+            }  
+
             return true;
         }
     }

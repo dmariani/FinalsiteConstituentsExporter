25ea3ccf5ad894b758f39181ecbe49cde9f5656b
unknown <dave@atscale.com>
2016-04-27 18:20:58 -0700

    reading xml for apilogin

--- FinalSiteConstituentsExporter/Main.cs
+++ FinalSiteConstituentsExporter/Main.cs
@@ -11,6 +11,8 @@ using System.Web;
 using System.Net.Http;
 using System.Net;
 using System.IO;
+using System.Xml;
+using System.Xml.XPath;
 
 namespace FinalSiteConstituentsExporter
 {
@@ -40,18 +42,21 @@ namespace FinalSiteConstituentsExporter
             DialogResult dialogResult = MessageBox.Show("Are you sure you want to begin the export starting at: " + this.Text, "Confirm Export", MessageBoxButtons.YesNo);
             if (dialogResult == DialogResult.Yes)
             {
-                if (ProcessExport(this.Text, textBoxDir.Text) == true)
+                String username = "david.mariani1";
+                String password = "Bully123";
+
+                try
                 {
-                    MessageBox.Show("Success!  Exported data starting at: " + this.Text + " and written to: " + textBoxDir.Text, "Export Results", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    GetAuthToken(username, password);
+
+                    long RowsExported = ProcessExport(this.Text, textBoxDir.Text);
+
+                    MessageBox.Show("Success!  Exported " + RowsExported.ToString() + " rows starting at: " + this.Text + " and wrote files to: " + textBoxDir.Text, "Export Results", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
-                else
+                catch (Exception ex)
                 {
-                    MessageBox.Show("Error!  Failed to export data starting at: " + this.Text + " and written to: " + textBoxDir.Text, "Export Results", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("Error: " + ex.Message, "Export Results", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-            }
-            else if (dialogResult == DialogResult.No)
-            {
-                //do something else
             }
         }
 
@@ -72,53 +77,88 @@ namespace FinalSiteConstituentsExporter
                 textBoxDir.Text = folderBrowserDialog1.SelectedPath;
             }
         }
-
-        private Boolean ProcessExport(String Datestr, String Dir)
+        
+        private String GetAuthToken(String Username, String Password)
         {
-            // We use the HttpUtility class from the System.Web namespace  
-            Uri address = new Uri("https://www.ola.community/api/apilogin/apilogin.cfm");
+            String Token = "";
 
-            // Create the web request  
-            HttpWebRequest request = WebRequest.Create(address) as HttpWebRequest;
-
-            // Set type to POST  
-            request.Method = "POST";
-            request.ContentType = "application/x-www-form-urlencoded";
-
-            string username = "david.mariani";
-            string password = "Bully123";
-
-            request.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.Default.GetBytes(username + ":" + password));
-
-            // Create the data we want to send  
-            string key = "0357D83A88AA320BE9194A00D753B5686CBC9547AFD0B6A7E54F9C84B064FBAD";
-
-            StringBuilder data = new StringBuilder();
-            data.Append("ApiEncryptedKey=" + HttpUtility.UrlEncode(key));
-
-            // Create a byte array of the data we want to send  
-            byte[] byteData = UTF8Encoding.UTF8.GetBytes(data.ToString());
-
-            // Set the content length in the request headers  
-            request.ContentLength = byteData.Length;
-
-            // Write data  
-            using (Stream postStream = request.GetRequestStream())
+            try
             {
-                postStream.Write(byteData, 0, byteData.Length);
+              // We use the HttpUtility class from the System.Web namespace  
+                Uri address = new Uri("https://www.ola.community/api/apilogin/apilogin.cfm");
+
+                // Create the web request  
+                HttpWebRequest request = WebRequest.Create(address) as HttpWebRequest;
+
+                // Set type to POST  
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.Default.GetBytes(Username + ":" + Password));
+
+                // Create the data we want to send  
+                string key = "0357D83A88AA320BE9194A00D753B5686CBC9547AFD0B6A7E54F9C84B064FBAD";
+
+                StringBuilder data = new StringBuilder();
+                data.Append("ApiEncryptedKey=" + HttpUtility.UrlEncode(key));
+
+                // Create a byte array of the data we want to send  
+                byte[] byteData = UTF8Encoding.UTF8.GetBytes(data.ToString());
+
+                // Set the content length in the request headers  
+                request.ContentLength = byteData.Length;
+
+                // Write data  
+                using (Stream postStream = request.GetRequestStream())
+                {
+                    postStream.Write(byteData, 0, byteData.Length);
+                }
+
+                // Get response  
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    // Get the response stream  
+//                    StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                    // Console application output  
+//                    Console.WriteLine(reader.ReadToEnd());
+
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(response.GetResponseStream());
+
+                    XmlNamespaceManager ns = new XmlNamespaceManager(doc.NameTable);
+                    ns.AddNamespace("apilogin", "http://www.finalsite.com/apidata/v1.0");
+
+                    XmlNode nodeSuccess = doc.SelectSingleNode("//ResultSet/OperationSucceeded", ns);
+                    if (nodeSuccess.InnerText != "True")
+                    {
+                        XmlNode nodeResult = doc.SelectSingleNode("//ResultSet/OperationResult", ns);
+                        throw new Exception("Error logging into API with user=" + Username + ". Error=" + nodeResult.InnerText);
+                    }
+                }
+
+                return Token;
             }
-
-            // Get response  
-            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            catch (WebException ex)
             {
-                // Get the response stream  
-                StreamReader reader = new StreamReader(response.GetResponseStream());
+                using (var stream = ex.Response.GetResponseStream())
+                using (var reader = new StreamReader(stream))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                    Exception newex = new Exception(reader.ToString());
+                    throw newex;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
 
-                // Console application output  
-                Console.WriteLine(reader.ReadToEnd());
-            }  
+        private long ProcessExport(String Datestr, String Dir)
+        {
+            long RowsExported = 0;
 
-            return true;
+            return RowsExported;
         }
     }
 }
